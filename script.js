748d4b005b725e1b6f25fddc7830e83c2269940d
nikosajadi <na.sajadii.29@gmail.com>
2022-09-13 14:18:45 +0200

    finally see our dark mode color

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ const toggleSwitch = document.querySelector('input[type="checkbox"]');
 
 //switch Theme Dynamically
 function switchTheme(event) {
-    console.log(event.target.checked);
+    // console.log(event.target.checked);
+    // if it is true
+    if (event.target.checked){
+        document.documentElement.setAttribute('data-theme','dark');
+    // if it is not checked,meaning if our checkbox cheched equals false
+    } else {
+        documentElement.documentElement.setAttribute('data-theme','light');
+    }
 }
 
 //event listener 
