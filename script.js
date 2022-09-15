43306ac179616a005057cacd9553d6815c93987c
nikosajadi <na.sajadii.29@gmail.com>
2022-09-15 14:23:12 +0200

    dark modelightLight Mode

--- script.js
+++ script.js
@@ -5,31 +5,44 @@ const toggleIcon = document.getElementById('toggle-icon');
 const image1 = document.getElementById('image1');
 const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
-const textBox   = document.getElementById('text-box');
-//Dark Mode style
+const textBox = document.getElementById('text-box');
+ 
+
+// Dark Mode Styles
 function darkMode() {
     nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    // console.log(toggleIcon.children);
-    toggleIcon.children[0].texContent = 'Dark Mode';
-    toggleIcon.children[1].classList.remove('fa-sun');
-    toggleIcon.children[1].classList.add('fa-moon');
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imageMode('dark');
+  }
 
-}
+// Light Mode Styles
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imageMode('light');
+  }
 
-//switch Theme Dynamically
+
+
+// Switch Theme Dynamically
 function switchTheme(event) {
-    // console.log(event.target.checked);
-    // if it is true
-    if (event.target.checked){
-        document.documentElement.setAttribute('data-theme','dark');
-        darkMode()
+    if (event.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+      darkMode();
     // if it is not checked,meaning if our checkbox cheched equals false
     } else {
-        document.documentElement.setAttribute('data-theme','light');
-        lightMode();
-    }
+       document.documentElement.setAttribute('data-theme', 'light');
+       localStorage.setItem('theme', 'light');
+       lightMode();
+  }
 }
 
-//event listener 
+
+
+//event listener = here we need to change event instead of clicking
 toggleSwitch.addEventListener('change', switchTheme); 
